bf203026982ce04da7f5a055c88df08c84d66641
Nicolas Chalono <nicolas.chalono@gmail.com>
2021-10-27 17:53:05 +0200

    update design on read file

--- read.php
+++ read.php
@@ -19,14 +19,19 @@ JOIN author on book.author_id = author.id";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
+  echo "<table><tr><th> ID</th><th> Titre</th><th> Auteur</th><th> Date de parution</th><th colspan=2> Requete</th></tr>";
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Titre: " . $row["title"]. " | Auteur " . $row["firstname"] ." ". $row["lastname"]. " " . $row["date"]. "<a href= 'update.php?id=".$row["id"]."'>  Editer</a>" . " ". "<a href= 'delete.php?id=".$row["id"]."'>  Supprimer</a><br>";
+    echo "<tr><td>" . $row["id"]. "</td><td>" . $row["title"]. "</td><td>" . $row["firstname"] ." ". $row["lastname"]. " </td><td>" . $row["date"]. "</td><td><a href= 'update.php?id=".$row["id"]."'>  Editer</a></td><td>" . " ". "<a href= 'delete.php?id=".$row["id"]."'>  Supprimer</a></td></td>";
   }
+  echo "</table>";
 } else {
   echo "0 results";
 }
 $conn->close();
+
+echo "<a href= 'create.php'>  Créer un livre </a></td></td>";
+
 
 
 
